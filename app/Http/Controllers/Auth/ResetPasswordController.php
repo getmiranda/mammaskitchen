d1f781d9f17c03401fdc62384fecc1be1f6b79e9
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2019-10-23 01:54:19 -0500

    Part 03: Redirect After Login

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/dasboard';
 
     /**
      * Create a new controller instance.
