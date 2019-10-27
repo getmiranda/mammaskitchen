157e8e8ed70281598d0b4a329f037e1a55b2d846
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2019-10-27 11:47:19 -0600

    Part 14: Item Edit and Delete

--- app/Http/Controllers/admin/ItemController.php
+++ app/Http/Controllers/admin/ItemController.php
@@ -90,7 +90,9 @@ class ItemController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = Item::findOrFail($id);
+        $categories = Category::all();
+        return view('admin.item.edit',compact('item','categories'));
     }
 
     /**
@@ -102,7 +104,37 @@ class ItemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'category' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'image' => 'mimes:jpeg,jpg,bmp,png',
+
+        ]);
+        $item = Item::findOrFail($id);
+        $image = $request->file('image');
+        $slug = str_slug($request->name);
+        if (isset($image))
+        {
+            $currentDate = Carbon::now()->toDateString();
+            $imagename = $slug.'-'.$currentDate.'-'. uniqid() .'.'. $image->getClientOriginalExtension();
+            if (!file_exists('uploads/item'))
+            {
+                mkdir('uploads/item',0777,true);
+            }
+            unlink('uploads/item/'.$item->image);
+            $image->move('uploads/item',$imagename);
+        }else{
+            $imagename = $item->image;
+        }
+        $item->category_id = $request->category;
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->image = $imagename;
+        $item->save();
+        return redirect()->route('item.index')->with('successMsg','Item successfully updated');
     }
 
     /**
@@ -113,6 +145,12 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::findOrFail($id);
+        if (file_exists('uploads/item/'.$item->image))
+        {
+            unlink('uploads/item/'.$item->image);
+        }
+        $item->delete();
+        return redirect()->back()->with('successMsg','Item successfully deleted');
     }
 }
