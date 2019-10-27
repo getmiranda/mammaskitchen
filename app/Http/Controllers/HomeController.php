bd3692f3e528b36d0c982c74101421bd2a8457df
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2019-10-27 13:03:17 -0600

    Part 15: Show Item by Category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-//use App\Category;
-//use App\Item;
+use App\Category;
+use App\Item;
 use App\Slider;
 use Illuminate\Http\Request;
 
@@ -17,8 +17,8 @@ class HomeController extends Controller
     public function index()
     {
         $sliders = Slider::all();
-        //$categories = Category::all();
-        //$items = Item::all();
-        return view('welcome',compact('sliders'));
+        $categories = Category::all();
+        $items = Item::all();
+        return view('welcome',compact('sliders','categories','items'));
     }
 }
